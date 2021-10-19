fc9bfe050ec6a9cf5fb24e38ab6351da697b1b77
Annas <61279556+annasajkh@users.noreply.github.com>
2021-10-19 00:17:12 +0000

    threading

--- main.py
+++ main.py
@@ -7,21 +7,37 @@ from matplotlib import cm
 
 import numpy as np
 import uvicorn
+import threading
+import requests
+import time
 
-app = FastAPI()
+def main_thread():
+  app = FastAPI()
 
-model = VAE()
-model.load_state_dict(torch.load("pixel_vae_model.pkl", map_location=torch.device("cpu")))
+  model = VAE()
+  model.load_state_dict(torch.load("pixel_vae_model.pkl", map_location=torch.device("cpu")))
 
-@app.get("/")
-def home():
-  return "Alive"
+  @app.get("/")
+  def home():
+    return "Alive"
 
-@app.get("/generate")
-async def main():
-  img = Image.fromarray(np.uint8(model.decode(torch.randn(1, 300)).detach().reshape(64, 64, 3) * 255))
-  img.resize((300,300), Image.NEAREST).save("generated.png")
-  return FileResponse("generated.png")
+  @app.get("/generate")
+  async def main():
+    img = Image.fromarray(np.uint8(model.decode(torch.randn(1, 300)).detach().reshape(64, 64, 3) * 255))
+    img.resize((300,300), Image.NEAREST).save("generated.png")
+    return FileResponse("generated.png")
 
-if __name__ == "__main__":
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+  if __name__ == "__main__":
+    while True:
+      try:
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+      except:
+        pass
+
+def ping_thread():
+  while True:
+    requests.get("https://Pixel-VAE.annasvirtual.repl.co")
+    time.sleep(3)
+
+threading.Thread(target=main_thread).start()
+threading.Thread(target=ping_thread).start()
